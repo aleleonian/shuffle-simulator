da9fc61b77379cd1feff50fd17ab620f5d3e818d
Alejandro Leonian <alejandromleonian@gmail.com>
2024-01-05 00:51:23 -0300

    Dialog implemented

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import DeckOrderDialog from './DeckOrderDialog';
 
 export function BottomMenu() {
+
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <div className='bottom-menu'>
             <ButtonGroup variant="contained" aria-label="outlined primary button group">
-                <Button>One</Button>
-                <Button>Two</Button>
-                <Button>Three</Button>
+                <Button onClick={handleClickOpen}>Deck Order</Button>
+                <Button>Shuffle Conf</Button>
+                <Button>About</Button>
             </ButtonGroup>
+
+            <DeckOrderDialog open={open}handleClose={handleClose} />
         </div>
     );
 }
