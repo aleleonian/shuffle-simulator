949dd0d2251845070c341a54726509ab8ac659f5
Alejandro Leonian <alejandromleonian@gmail.com>
2024-01-09 23:30:38 -0300

    shuffle conf state change is reflected in configuration panel

--- src/components/ConfigurationInfo.jsx
+++ src/components/ConfigurationInfo.jsx
@@ -5,15 +5,21 @@ import { Grid, TextField } from "@mui/material";
 export function ConfigurationInfo() {
 
     const myContext = useStateContext();
-
+    console.log(myContext);
     return (
         <Grid item xs={12}>
             <div className="configuration-info">
                 <TextField
                     label="Deck order"
-                    id="outlined-start-adornment"
+                    id="do-outlined-start-adornment"
                     sx={{ m: 1, width: '25ch' }}
                     value={myContext.deckOrderState.title}
+                />
+                <TextField
+                    label="Shuffle Conf"
+                    id="sc-outlined-start-adornment"
+                    sx={{ m: 1, width: '25ch' }}
+                    value={myContext.shuffleConfState}
                 />
             </div>
         </Grid>
