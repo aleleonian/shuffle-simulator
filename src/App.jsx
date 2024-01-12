14413cbe5d590258e9e38a78f9eb62521c0167c0
Alejandro Leonian <alejandromleonian@gmail.com>
2024-01-12 20:04:44 -0300

    modified primary and secondary colors via theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import { Header } from "./components/Header";
 import { Body } from "./components/Body";
-import { Menu } from './components/Menu';
-import SimpleBottomNavigation from './components/SimpleBottomNavigation';
+// import { Menu } from './components/Menu';
 import { StateProvider } from './components/StateContext';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { blue } from '@mui/material/colors';
+
+
+const black = {
+  main: '#000',
+  light: '#ccc',
+  dark: '#000',
+  contrastText: '#fff',
+};
+
+
+const theme = createTheme({
+  typography: {
+    "fontFamily": `"Helvetica", "Arial", sans-serif`,
+  },
+  palette: {
+    primary: black,
+    secondary: blue,
+  },
+});
 
 import './assets/index.css';
 
@@ -11,9 +31,11 @@ function App() {
   return (
     <>
       <StateProvider>
+        <ThemeProvider theme={theme}>
           {/* <Menu /> */}
           <Header />
           <Body />
+        </ThemeProvider>
       </StateProvider>
     </>
   )
