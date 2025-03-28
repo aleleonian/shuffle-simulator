46c60bbee6f135955da6c0b8a7eed4c77f75cc65
Alejandro Leonian <alejandromleonian@gmail.com>
2025-03-28 10:55:33 -0300

    custom deck chosen bug fixed

--- src/components/ConfigurationInfo.jsx
+++ src/components/ConfigurationInfo.jsx
@@ -29,7 +29,18 @@ export function ConfigurationInfo() {
     }
 
     function deckOrderHandler(event) {
+        debugger;
         const chosenOrder = event.target.value;
+        let chosenStack = { ...stacks.find(stack => stack.name == chosenOrder) };
+        //chosenStack can be an empty object if 'custom' is chosen for the first time.
+        //in that case, we should initialize the object
+        const isEmpty = Object.keys(chosenStack).length === 0;
+        if (isEmpty) {
+            chosenStack.name = "other";
+            chosenStack.title = "custom";
+            chosenStack.order = [];
+        }
+        myContext.updateDeckOrderState(chosenStack);
 
         switch (chosenOrder) {
             case false:
@@ -39,9 +50,7 @@ export function ConfigurationInfo() {
                 return setDeckBuilderOpen(true)
         }
 
-        const chosenStack = { ...stacks.find(stack => stack.name == chosenOrder) };
 
-        myContext.updateDeckOrderState(chosenStack);
     }
 
     function shuffleConfHandler(event) {
